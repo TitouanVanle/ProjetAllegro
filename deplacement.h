5b332da9a0078e0d01f11c26161c8c606cdade9b
TitouanVanle <96185930+TitouanVanle@users.noreply.github.com>
2022-05-22 15:02:05 +0200

    Create deplacement.h

---
+++ deplacement.h
@@ -0,0 +1,12 @@
+#ifndef DEPLACEMENT_H_INCLUDED
+#define DEPLACEMENT_H_INCLUDED
+
+#include "mesfonctions.h"
+
+void afficheDeplacement(BITMAP * page,t_joueur* J);
+void sedeplacer(t_joueur* J,BITMAP * fond, BITMAP *page);
+
+
+
+
+#endif // DEPLACEMENT_H_INCLUDED
